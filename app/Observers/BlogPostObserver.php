50354087ac8c3bf501666bf0058221c9dda7bb25
Ira-name <iryna.krush@oa.edu.ua>
2024-05-30 11:41:58 +0300

    Lab_10. Adding and deleting articles

--- app/Observers/BlogPostObserver.php
+++ app/Observers/BlogPostObserver.php
@@ -9,6 +9,47 @@ use Illuminate\Support\Str;
 class BlogPostObserver
 {
     /**
+     * Обробка перед створенням запису.
+     *
+     * @param  BlogPost  $blogPost
+     * 
+     */
+    public function creating(BlogPost $blogPost)
+    {
+        $this->setPublishedAt($blogPost);
+
+        $this->setSlug($blogPost);
+
+        $this->setHtml($blogPost);
+
+        $this->setUser($blogPost);
+    }
+
+/**
+     * Встановлюємо значення полю content_html з поля content_raw.
+     * 
+     * @param BlogPost $blogPost
+     */
+    protected function setHtml(BlogPost $blogPost)
+    {
+        if ($blogPost->isDirty('content_raw')) {
+            //Тут треба зробити генерацію markdown -> html
+            $blogPost->content_html = $blogPost->content_raw;
+        }
+    }
+
+    /**
+     * Якщо user_id не вказано, то встановимо юзера 1.
+     * 
+     * @param BlogPost $blogPost
+     */
+    protected function setUser(BlogPost $blogPost)
+    {
+        
+        $blogPost->user_id = auth()->id() ?? BlogPost::UNKNOWN_USER;
+        
+    }
+    /**
      * Handle the BlogPost "created" event.
      */
     /**
@@ -30,6 +71,7 @@ class BlogPostObserver
      * 
      * @param BlogPost $blogPost
      */
+    
     protected function setPublishedAt(BlogPost $blogPost)
     {
         if (empty($blogPost->published_at) && $blogPost->is_published) {
